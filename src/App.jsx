2c3badbb3ad6243091ebc4745daf0400bf8ce324
ponochovny <ponochovny@gmail.com>
2026-05-16 19:02:29 +0300

    Add Final and PostCard components with GSAP animations; update App structure and CSS styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import FirstVideo from './sections/FirstVideo'
 import Jason from './sections/Jason'
 import SecondVideo from './sections/SecondVideo'
 import Lucia from './sections/Lucia'
+import PostCard from './sections/PostCard'
+import Final from './sections/Final'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -19,6 +21,8 @@ const App = () => {
 			<Jason />
 			<SecondVideo />
 			<Lucia />
+			<PostCard />
+			<Final />
 		</main>
 	)
 }
---
+++ src/sections/Final.tsx
@@ -0,0 +1,70 @@
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { useRef } from 'react'
+
+const Final = () => {
+	const videoRef = useRef<HTMLVideoElement>(null)
+
+	useGSAP(() => {
+		gsap.set('.final-content', { opacity: 0 })
+
+		gsap.timeline({
+			scrollTrigger: {
+				trigger: '.final',
+				start: 'top top',
+				end: '90% top',
+				scrub: 2,
+				pin: true,
+			},
+		})
+
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: '.final',
+				start: 'top 80%',
+				end: '90% top',
+				scrub: 2,
+			},
+		})
+
+		tl.to('.final-content', {
+			opacity: 1,
+			duration: 1,
+			scale: 1,
+			ease: 'power1.inOut',
+		})
+
+		if (videoRef.current) {
+			videoRef.current.onloadedmetadata = () => {
+				if (!videoRef.current) return
+
+				tl.to(
+					videoRef.current,
+					{
+						currentTime: videoRef.current.duration,
+						duration: 3,
+						ease: 'power1.inOut',
+					},
+					'<',
+				)
+			}
+		}
+	})
+
+	return (
+		<section className='final'>
+			<div className='final-content size-full'>
+				<video
+					ref={videoRef}
+					src='/videos/output3.mp4'
+					className='size-full object-cover'
+					muted
+					playsInline
+					preload='auto'
+				/>
+			</div>
+		</section>
+	)
+}
+
+export default Final
